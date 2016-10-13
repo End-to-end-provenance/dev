3c13c73a29e716528f219f4163b7bdb7cacf2c5e
MKLau <matthewklau@fas.harvard.edu>
2016-10-13 16:22:00 -0400

    add ody directory to slrum sh

--- src/eepOdy/src/run.sh
+++ src/eepOdy/src/run.sh
@@ -8,5 +8,6 @@
 #SBATCH -e hostname_%j.err      # File to which STDERR will be written
 #SBATCH --mail-type=ALL         # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user=matthewklau@fas.harvard.edu # Email to which notifications will be sent
+#SBATCH -D ~/regal/mklau/dev/src/eepOdy/src
 
 Rscript run.R $SLURM_JOBID
